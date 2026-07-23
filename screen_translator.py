84746263659bfb2cf46b5dbc299aba7d771bb767
Rayy <137747946+Rayy-create@users.noreply.github.com>
2026-07-23 22:28:37 +0700

    Remove installation instructions from screen_translator.py
    Removed installation instructions from the top of the file.

--- screen_translator.py
+++ screen_translator.py
@@ -1,27 +1,3 @@
-"""
-SCREEN TRANSLATOR OTOMATIS (v2 - akurasi & tampilan ditingkatkan)
-==================================================================
-
-CARA INSTALL (WAJIB DIBACA):
-1. Install Python 3.9+ dari https://www.python.org/downloads/
-   (saat install, centang "Add Python to PATH")
-
-2. Install Tesseract OCR:
-   - Download installer Windows di:
-     https://github.com/UB-Mannheim/tesseract/wiki
-   - PENTING: saat instalasi, di layar "Choose Components" pastikan
-     centang paket bahasa yang Anda butuhkan (misalnya Indonesian,
-     Japanese, Korean, Chinese) selain English -- defaultnya cuma
-     English yang aktif.
-   - Install di lokasi default (C:\\Program Files\\Tesseract-OCR)
-
-3. Buka Command Prompt di folder ini, install library Python:
-   pip install mss pytesseract pillow deep-translator
-
-4. Jalankan:
-   python screen_translator.py
-"""
-
 import os
 import re
 import threading
